2c232d93ab70137eed8897ecc8170f945658008f
renhaojheng <renhaojheng@gmail.com>
2022-02-09 23:48:50 +0800

    Setup MongoDB database

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const Shorten = require('./models/shorten')
+
+monogoose = require('./config/mongoose')
 
 const app = express()
 const port = 3000
 
-app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: 'hbs' }))
+app.set('view engine', 'hbs')
+
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.post('/shorten', (req, res) => {
+  const { url } = req.body
+  return Shorten.create({ inputURL: url })
+    .then(shorten => res.render('show', { url: shorten.inputURL }))
+    .catch(error => console.log(error))
 })
 
 app.listen(port, () => {
---
+++ models/shorten.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const shortenSchema = new Schema({
+  inputURL: {
+    type: String,
+    required: true
+  },
+  outputText: {
+    type: String,
+    // required: true
+  }
+})
+
+module.exports = mongoose.model('Shorten', shortenSchema)
